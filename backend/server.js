f54a32cba59502e5ad89fe96a1decabb204f61da
Maria Sparre <you@example.com>
2021-05-31 23:09:21 +0200

    created model User

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,50 @@ import cors from 'cors'
 import mongoose from 'mongoose'
 import listEndpoints from 'express-list-endpoints'
 import dotenv from 'dotenv'
+//mongoose-type-email ?? 
 
 dotenv.config()
 
 const mongoUrl = process.env.MONGO_URL || `mongodb+srv://${process.env.USER_ID}:${process.env.API_KEY}@cluster0.ekh6z.mongodb.net/habitTracker?retryWrites=true&w=majority`
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
 mongoose.Promise = Promise
+
+const userSchema = new mongoose.Schema ({
+  username: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: mongoose.SchemaTypes.Email,
+    trim: true,
+    lowercase: true,
+    unique: [true, 'Sorry, that email is already in use'],
+    required: [true, 'Email is required']
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString('hex')
+  }
+})
+const User = mongoose.model('User', userSchema)
+
+// const authenticateUser = async (req, res, next) => {
+//   const accessToken = req.header('Authorization')
+//   try {
+//     const user = await User.findOne({ accessToken })
+//     if (user) {
+//       next()
+//     } else {
+//       res.status(401).json({ success: false, message: 'Not authorized' })
+//     }
+//   } catch (error) {
+//     res.status(400).json({ success: false, message: 'Invalid request', error })
+//   }
+// }
 
 const port = process.env.PORT || 8080
 const app = express()
@@ -22,6 +60,44 @@ app.get('/', (req, res) => {
   res.send(listEndpoints(app))
 })
 
+app.post('/signup', async (req, res) => {
+  const { username, password } = req.body
+  try {
+    const salt = bcrypt.genSaltSync()
+    const newUser = await new User({
+      username,
+      password: bcrypt.hashSync(password, salt)
+    }).save()
+    res.json({
+      success: true,
+      userID: newUser._id,
+      username: newUser.username,
+      accessToken: newUser.accessToken
+    })
+  } catch (error) {
+    res.status(400).json({ success: false, message: 'Invalid request', error })
+  }
+})
+
+app.post('/signin', async (req, res) => {
+  const { username, password } = req.body
+
+  try {
+    const user = await User.findOne({ username })
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.json({
+        success: true, 
+        userID: user._id,
+        username: user.username,
+        accessToken: user.accessToken
+      })
+    } else {
+      res.status(404).json({ success: false, message: 'User not found' })
+    }
+  } catch (error) {
+    res.status(400).json({ success: false, message: 'Invalid request', error })
+  }
+})
 // Start the server
 app.listen(port, () => {
   // eslint-disable-next-line
