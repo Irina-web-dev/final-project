02384c51edf8bb975716c8a8af6f1fb7bddd8860
Maria Sparre <you@example.com>
2021-06-08 09:34:30 +0200

    styled signin and signup form mobile view

--- frontend/src/pages/SignIn.js
+++ frontend/src/pages/SignIn.js
@@ -52,21 +52,93 @@ const SignIn = () => {
       .catch()
   }
 
+  const Container = styled.div`
+    margin: 30px;
+    height: 70vh; 
+    max-width: 100%; 
+    box-shadow: 
+      0 14px 28px rgba(0, 0, 0, .2), 
+      0 10px 10px rgba(0, 0, 0, .2);
+  `
+  const SignUpButton = styled.button`
+    width: 100%;
+    font-size: 30px; 
+    outline: none; 
+    background: none; 
+    border: none; 
+    font-size: 20px; 
+  `
+  const Form = styled.form`
+    display: flex;
+    flex-direction: column; 
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    height: 100%;
+    padding: 0 50px;
+  `
+  const TitleText = styled.h1`
+   font-size: 20px; 
+  `
+  const ParagrafText = styled.p`
+    letter-spacing: 0,5px;
+  `
+  const Button = styled.button`
+    border-radius: 20px;
+    border: 1px solid #eee;
+    width: 80%;
+    color: #fff; 
+    background: #407294;
+    padding: 5px; 
+    font-size: 12px;
+    font-weight: bold;
+    letter-spacing: 1px;
+    text-transform: uppercase;
+    cursor: pointer;
+    transition: transform 80ms ease-in;
+
+    &:active {
+      transform: scale(.95);
+    } 
+  ` 
+  const InputArea = styled.input`
+    background: #eee;
+    border: none;
+    padding: 12px 15px;
+    margin: 8px 0;
+    width: 100%;
+  `
+  const TitleContainer = styled.div`
+    display: flex; 
+  `
+  const Separator = styled.span`
+    font-size: 80px; 
+  `
+
   return (
-    <form onSubmit={onFormSubmit}>
-      <input
-        type="text"
-        value={usernameOrEmail}
-        onChange={(e) => setUsernameOrEmail(e.target.value)}
-      />
-      <input
-         type="password"
-         value={password}
-         onChange={(e) => setPassword(e.target.value)}
-      />
-      {errors? <p>{errors.message}</p> : ''}
-      <button type='submit'>Sign in</button>
-    </form>
+    <Container>
+      <Form onSubmit={onFormSubmit}>
+        <TitleContainer>
+          <TitleText>Sign In</TitleText>
+          <SignUpButton> <Separator>|</Separator> Sign Up</SignUpButton>
+        </TitleContainer>
+          <InputArea
+            type="text"
+            placeholder="username or email.."
+            value={usernameOrEmail}
+            onChange={(e) => setUsernameOrEmail(e.target.value)}
+          />
+          <InputArea
+            type="password"
+            placeholder="Password.."
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          {errors? <p>{errors.message}</p> : ''}
+          <ParagrafText>Forgot password?</ParagrafText>
+          <Button type='submit'>Sign in</Button>
+      </Form>
+    </Container>
   )
 }
 
--- frontend/src/pages/SignUp.js
+++ frontend/src/pages/SignUp.js
@@ -48,33 +48,96 @@ const SignUp = () => {
       .catch()
   }
 
+  const Container = styled.div`
+  margin: 30px;
+  height: 70vh; 
+  max-width: 100%; 
+  box-shadow: 
+    0 14px 28px rgba(0, 0, 0, .2), 
+    0 10px 10px rgba(0, 0, 0, .2);
+`
+const SignInButton = styled.button`
+  width: 100%;
+  font-size: 30px; 
+  outline: none; 
+  background: none; 
+  border: none; 
+  font-size: 20px; 
+`
+const Form = styled.form`
+  display: flex;
+  flex-direction: column; 
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  height: 100%;
+  padding: 0 50px;
+`
+const TitleText = styled.h1`
+ font-size: 20px; 
+`
+const Button = styled.button`
+  border-radius: 20px;
+  border: 1px solid #eee;
+  width: 80%;
+  color: #fff; 
+  background: #407294;
+  padding: 5px; 
+  font-size: 12px;
+  font-weight: bold;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  cursor: pointer;
+  transition: transform 80ms ease-in;
+
+  &:active {
+    transform: scale(.95);
+  } 
+` 
+const InputArea = styled.input`
+  background: #eee;
+  border: none;
+  padding: 12px 15px;
+  margin: 8px 0;
+  width: 100%;
+`
+const TitleContainer = styled.div`
+  display: flex; 
+`
   return (
-    <>
+    <Container>
     {!accessToken 
       ?
-      <form onSubmit={onFormSubmit}>
-      <input
+      <Form onSubmit={onFormSubmit}>
+        <TitleContainer>
+          <TitleText>Sign Up </TitleText>
+          <SignInButton> <span>|</span> Sign In</SignInButton>
+        </TitleContainer>
+      <InputArea
         type="text"
+        placeholder="username.."
         value={username}
         onChange={(e) => setUsername(e.target.value)}
       />
-      <input
+      <InputArea
         type="text"
+        placeholder="email"
         value={email}
         onChange={(e) => setEmail(e.target.value)}
       />
-      <input
+      <InputArea
          type="password"
+         placeholder="password"
          value={password}
          onChange={(e) => setPassword(e.target.value)}
       />
       {errors? <p>{errors.message}</p> : ''}
-      <button type='submit'>Sign up</button>
-    </form>
+      <Button type='submit'>Sign up</Button>
+    </Form>
     :
     <div>You have an account now!🎇 Sign in here <span role='img' aria-label='finger pointing'>👉👉👉</span><Link to="/signin">Sign in</Link></div>
     }
-    </>
+    </Container>
   )
 }
 
