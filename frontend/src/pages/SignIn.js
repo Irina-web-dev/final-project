af077aeedbd84c520ff699ca25ff05e1de0d9c91
Maria Sparre <you@example.com>
2021-06-09 00:25:26 +0200

    styled signin and sign up component, added Link

--- frontend/src/pages/SignIn.js
+++ frontend/src/pages/SignIn.js
@@ -1,11 +1,101 @@
 import React, { useState, useEffect } from 'react'
 import { useDispatch, batch, useSelector } from 'react-redux'
-import { useHistory } from 'react-router-dom'
+import { useHistory, Link } from 'react-router-dom'
 import styled from 'styled-components/macro'
 
 import user from '../reducers/user'
 
 import { API_URL } from '../reusable/urls'
+
+const Container = styled.div`
+margin: 30px;
+height: 70vh; 
+max-width: 100%; 
+box-shadow: 
+  0 14px 28px rgba(0, 0, 0, .2), 
+  0 10px 10px rgba(0, 0, 0, .2);
+
+@media (max-width: 1024px) {
+  max-width: 600px; 
+  display: flex; 
+}
+`
+const StyledLink = styled(Link)`
+width: 100%;
+font-size: 30px;  
+outline: none; 
+background: none; 
+border: none; 
+font-size: 20px; 
+color: #D21F3C; 
+text-decoration: none; 
+margin-left: 5px; 
+
+@media (min-width: 768px) {
+ 
+}
+`
+const Form = styled.form`
+display: flex;
+flex-direction: column; 
+justify-content: center;
+align-items: center;
+text-align: center;
+height: 100%;
+padding: 0 50px;
+
+@media (min-width: 768px) {
+  padding: 0 70px;
+}
+`
+const TitleText = styled.h1`
+font-size: 20px; 
+text-transform: uppercase;
+text-align: left; 
+
+@media (max-width: 1024px) {
+font-size: 25px;  
+}
+`
+const ParagrafText = styled.p`
+letter-spacing: 0,5px;
+`
+const Button = styled.button`
+border-radius: 20px;
+border: 1px solid #eee;
+width: 80%;
+color: #fff; 
+background: #407294;
+padding: 5px; 
+margin: 10px; 
+font-size: 12px;
+font-weight: bold;
+letter-spacing: 1px;
+text-transform: uppercase;
+cursor: pointer;
+transition: transform 80ms ease-in;
+
+&:active {
+  transform: scale(.95);
+} 
+` 
+const InputArea = styled.input`
+background: #eee;
+border: none;
+padding: 12px 15px;
+margin: 8px 0;
+width: 100%;
+
+::placeholder {
+  color: #D21F3C;
+}
+`
+const TitleContainer = styled.div`
+display: flex; 
+`
+const Separator = styled.span`
+font-size: 40px; 
+`
 
 const SignIn = () => {
   const [usernameOrEmail, setUsernameOrEmail] = useState('')
@@ -51,90 +141,23 @@ const SignIn = () => {
       })
       .catch()
   }
-
-  const Container = styled.div`
-    margin: 30px;
-    height: 70vh; 
-    max-width: 100%; 
-    box-shadow: 
-      0 14px 28px rgba(0, 0, 0, .2), 
-      0 10px 10px rgba(0, 0, 0, .2);
-  `
-  const SignUpButton = styled.button`
-    width: 100%;
-    font-size: 40px; 
-    outline: none; 
-    background: none; 
-    border: none; 
-    font-size: 20px; 
-
-    &:after {
-      
-    }
-  `
-  const Form = styled.form`
-    display: flex;
-    flex-direction: column; 
-    justify-content: center;
-    align-items: center;
-    text-align: center;
-    height: 100%;
-    padding: 0 50px;
-  `
-  const TitleText = styled.h1`
-   font-size: 30px; 
-   text-align: left; 
-  `
-  const ParagrafText = styled.p`
-    letter-spacing: 0,5px;
-  `
-  const Button = styled.button`
-    border-radius: 20px;
-    border: 1px solid #eee;
-    width: 80%;
-    color: #fff; 
-    background: #407294;
-    padding: 5px; 
-    margin: 10px; 
-    font-size: 12px;
-    font-weight: bold;
-    letter-spacing: 1px;
-    text-transform: uppercase;
-    cursor: pointer;
-    transition: transform 80ms ease-in;
-
-    &:active {
-      transform: scale(.95);
-    } 
-  ` 
-  const InputArea = styled.input`
-    background: #eee;
-    border: none;
-    padding: 12px 15px;
-    margin: 8px 0;
-    width: 100%;
-  `
-  const TitleContainer = styled.div`
-    display: flex; 
-  `
-  const Separator = styled.span`
-    font-size: 50px; 
-  `
-
+ 
   return (
     <Container>
       <Form onSubmit={onFormSubmit}>
         <TitleContainer>
           <TitleText>Sign In</TitleText>
-          <SignUpButton> <Separator>|</Separator> Sign Up</SignUpButton>
+          <StyledLink to="/signup"> <Separator>|</Separator> Sign Up</StyledLink>
         </TitleContainer>
           <InputArea
+            required
             type="text"
             placeholder="username or email.."
             value={usernameOrEmail}
             onChange={(e) => setUsernameOrEmail(e.target.value)}
           />
           <InputArea
+            required
             type="password"
             placeholder="password.."
             value={password}
@@ -142,7 +165,7 @@ const SignIn = () => {
           />
           {errors? <p>{errors.message}</p> : ''}
           <ParagrafText>Forgot password?</ParagrafText>
-          <Button type='submit'>Sign in</Button>
+          <Button type="submit">Sign in</Button>
       </Form>
     </Container>
   )
--- frontend/src/pages/SignUp.js
+++ frontend/src/pages/SignUp.js
@@ -7,6 +7,97 @@ import user from '../reducers/user'
 
 import { API_URL } from '../reusable/urls'
 
+const Container = styled.div`
+  margin: 30px;
+  height: 70vh; 
+  max-width: 100%; 
+  box-shadow: 
+    0 14px 28px rgba(0, 0, 0, .2), 
+    0 10px 10px rgba(0, 0, 0, .2);
+
+    @media (max-width: 1024px) {
+      max-width: 600px; 
+      display: flex; 
+    }
+`
+const StyledLink = styled(Link)`
+    width: 100%;
+    font-size: 30px; 
+    outline: none; 
+    background: none; 
+    border: none; 
+    font-size: 20px; 
+    color: #D21F3C; 
+    text-decoration: none; 
+    margin-left: 5px; 
+
+    @media (min-width: 768px) {
+      
+    }
+  `
+const Form = styled.form`
+  display: flex;
+  flex-direction: column; 
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  height: 100%;
+  padding: 0 50px;
+
+  @media (min-width: 768px) {
+    padding: 0 70px;
+  }
+`
+const TitleText = styled.h1`
+ font-size: 20px; 
+ text-align: left; 
+ text-transform: uppercase;
+
+ @media (max-width: 1024px) {
+  font-size: 25px;  
+}
+`
+const Button = styled.button`
+  border-radius: 20px;
+  border: 1px solid #eee;
+  width: 80%;
+  color: #fff; 
+  background: #407294;
+  padding: 5px; 
+  margin: 10px; 
+  font-size: 12px;
+  font-weight: bold;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  cursor: pointer;
+  transition: transform 80ms ease-in;
+
+  &:active {
+    transform: scale(.95);
+  } 
+` 
+const InputArea = styled.input`
+  background: #eee;
+  border: none;
+  padding: 12px 15px;
+  margin: 8px 0;
+  width: 100%;
+
+  ::placeholder {
+    color: #D21F3C;
+  }
+
+  @media (max-width: 1024px) {
+    width: 80%;  
+  }
+`
+const TitleContainer = styled.div`
+  display: flex; 
+`
+const Separator = styled.span`
+  font-size: 40px; 
+`
+
 const SignUp = () => {
   const [username, setUsername] = useState('')
   const [email, setEmail] = useState('')
@@ -48,67 +139,6 @@ const SignUp = () => {
       .catch()
   }
 
-const Container = styled.div`
-  margin: 30px;
-  height: 70vh; 
-  max-width: 100%; 
-  box-shadow: 
-    0 14px 28px rgba(0, 0, 0, .2), 
-    0 10px 10px rgba(0, 0, 0, .2);
-`
-const SignInButton = styled.button`
-  width: 100%;
-  font-size: 30px; 
-  outline: none; 
-  background: none; 
-  border: none; 
-  font-size: 20px; 
-`
-const Form = styled.form`
-  display: flex;
-  flex-direction: column; 
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-  height: 100%;
-  padding: 0 50px;
-`
-const TitleText = styled.h1`
- font-size: 30px; 
- text-align: left; 
-`
-const Button = styled.button`
-  border-radius: 20px;
-  border: 1px solid #eee;
-  width: 80%;
-  color: #fff; 
-  background: #407294;
-  padding: 5px; 
-  font-size: 12px;
-  font-weight: bold;
-  letter-spacing: 1px;
-  text-transform: uppercase;
-  cursor: pointer;
-  transition: transform 80ms ease-in;
-
-  &:active {
-    transform: scale(.95);
-  } 
-` 
-const InputArea = styled.input`
-  background: #eee;
-  border: none;
-  padding: 12px 15px;
-  margin: 8px 0;
-  width: 100%;
-`
-const TitleContainer = styled.div`
-  display: flex; 
-`
-const Separator = styled.span`
-  font-size: 40px; 
-`
-
   return (
     <Container>
     {!accessToken 
@@ -116,21 +146,24 @@ const Separator = styled.span`
       <Form onSubmit={onFormSubmit}>
         <TitleContainer>
           <TitleText>Sign Up </TitleText>
-          <SignInButton> <Separator>|</Separator> Sign In</SignInButton>
+          <StyledLink to="/signin"> <Separator>|</Separator> Sign In</StyledLink>
         </TitleContainer>
       <InputArea
+        required
         type="text"
         placeholder="username.."
         value={username}
         onChange={(e) => setUsername(e.target.value)}
       />
       <InputArea
+        required
         type="text"
         placeholder="email"
         value={email}
         onChange={(e) => setEmail(e.target.value)}
       />
       <InputArea
+         required
          type="password"
          placeholder="password"
          value={password}
@@ -140,7 +173,7 @@ const Separator = styled.span`
       <Button type='submit'>Sign up</Button>
     </Form>
     :
-    <div>You have an account now!🎇 Sign in here <span role='img' aria-label='finger pointing'>👉👉👉</span><Link to="/signin">Sign in</Link></div>
+    <div>You have an account now!<Link to="/signin">Sign in</Link></div>
     }
     </Container>
   )
